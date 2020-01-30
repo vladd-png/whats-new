5a0f718243a1d54c9e7fbfe48ea06e06bcf83d53
Virginia Ladd <virginialadd2@gmail.com>
2020-01-29 17:20:48 -0700

    Create Menu class component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
+
+class Menu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newsType: ''
+    }
+  }
+  render() {
+    return (
+      <nav>
+        <h1>What's New</h1>
+        <ul>
+          <li><img src='./image.png' alt='news icon' />Local News</li>
+          <li><img src='./image.png' alt='news icon' />Technology</li>
+          <li><img src='./image.png' alt='news icon' />Entertainment</li>
+          <li><img src='./image.png' alt='news icon' />Science</li>
+          <li><img src='./image.png' alt='news icon' />Health</li>
+        </ul>
+      </nav>
+    )
+  }
+}
 
 export default Menu;
