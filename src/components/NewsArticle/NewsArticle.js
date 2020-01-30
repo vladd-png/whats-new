7555c5af8b168b60f96dbad051eb7cf19e9760d6
Virginia Ladd <virginialadd2@gmail.com>
2020-01-29 17:19:45 -0700

    Create NewsArticle component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
-//code to make a commit
+const NewsArticle = (props) => {
+  return (
+    <section className='newsCard'>
+    <section className='imgContainer'>
+      <img src={props.img} alt={props.headline}/>
+    </section>
+      <h1 className='newsHeadline'>{props.headline}</h1>
+      <p className='newsDescription'>{props.body}</p>
+      <footer className='footerInfo'>
+        <a className='urlLink' href={props.link}>Link To Article</a>
+        <img className='arrowImg' src='../../../../public/arrow.png' alt='arrow icon'/>
+      </footer>
+    </section>
+  )
+}
 
 export default NewsArticle;
