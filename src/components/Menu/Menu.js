fa7f976a38f81365b656355699814f5815e129b0
Virginia Ladd <virginialadd2@gmail.com>
2020-01-29 18:59:52 -0700

    Menu styling

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import './Menu.css'
+import './Menu.css';
+import location from '../../loaction.png';
+import technology from '../../lightbulb.png';
+import entertainment from '../../tickets.png';
+import science from '../../rocket.png';
+import health from '../../health.png';
 
-// MENU COMPONENT CODE GOES HERE
 
 class Menu extends Component {
   constructor() {
     super();
     this.state = {
-      newsType: ''
+      newsType: 'local'
     }
+  }
+  handleChange = (newsString) => {
+    this.setState({newsType: newsString});
   }
   render() {
     return (
       <nav>
         <h1>What's <span className='newsTitle'>New ?</span></h1>
         <ul>
-          <li className='newsType'><img src='./image.png' alt='news icon' />Local News</li>
-          <li className='newsType'><img src='./image.png' alt='news icon' />Technology</li>
-          <li className='newsType'><img src='./image.png' alt='news icon' />Entertainment</li>
-          <li className='newsType'><img src='./image.png' alt='news icon' />Science</li>
-          <li className='newsType'><img src='./image.png' alt='news icon' />Health</li>
+          <li className='newsType' onClick={() => this.handleChange('local')}><img className='imgIcon' src={location} alt='news icon' />Local News</li>
+          <li className='newsType' onClick={() => this.handleChange('technology')}><img className='imgIcon' src={technology} alt='news icon' />Technology</li>
+          <li className='newsType' onClick={() => this.handleChange('entertainment')}><img className='imgIcon' src={entertainment} alt='news icon' />Entertainment</li>
+          <li className='newsType' onClick={() => this.handleChange('science')}><img className='imgIcon' src={science} alt='news icon' />Science</li>
+          <li className='newsType' onClick={() => this.handleChange('health')}><img className='imgIcon' src={health} alt='news icon'/>Health</li>
         </ul>
       </nav>
     )
