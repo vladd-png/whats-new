aaec624a8dd9b30d1012d18634809304784e1585
Virginia Ladd <virginialadd2@gmail.com>
2020-01-29 20:02:14 -0700

    added an onClick function to the menu list items

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Menu.css';
 import location from '../../loaction.png';
 import technology from '../../lightbulb.png';
@@ -7,30 +7,19 @@ import science from '../../rocket.png';
 import health from '../../health.png';
 
 
-class Menu extends Component {
-  constructor() {
-    super();
-    this.state = {
-      newsType: 'local'
-    }
-  }
-  handleChange = (newsString) => {
-    this.setState({newsType: newsString});
-  }
-  render() {
+const Menu = props => {
     return (
       <nav>
         <h1>What's <span className='newsTitle'>New ?</span></h1>
         <ul>
-          <li className='newsType' onClick={() => this.handleChange('local')}><img className='imgIcon' src={location} alt='news icon' />Local News</li>
-          <li className='newsType' onClick={() => this.handleChange('technology')}><img className='imgIcon' src={technology} alt='news icon' />Technology</li>
-          <li className='newsType' onClick={() => this.handleChange('entertainment')}><img className='imgIcon' src={entertainment} alt='news icon' />Entertainment</li>
-          <li className='newsType' onClick={() => this.handleChange('science')}><img className='imgIcon' src={science} alt='news icon' />Science</li>
-          <li className='newsType' onClick={() => this.handleChange('health')}><img className='imgIcon' src={health} alt='news icon'/>Health</li>
+          <li className='newsType' onClick={() => props.handleChange('local')}><img className='imgIcon' src={location} alt='news icon' />Local News</li>
+          <li className='newsType' onClick={() => props.handleChange('technology')}><img className='imgIcon' src={technology} alt='news icon' />Technology</li>
+          <li className='newsType' onClick={() => props.handleChange('entertainment')}><img className='imgIcon' src={entertainment} alt='news icon' />Entertainment</li>
+          <li className='newsType' onClick={() => props.handleChange('science')}><img className='imgIcon' src={science} alt='news icon' />Science</li>
+          <li className='newsType' onClick={() => props.handleChange('health')}><img className='imgIcon' src={health} alt='news icon'/>Health</li>
         </ul>
       </nav>
     )
-  }
 }
 
 export default Menu;
